0b0f284ea0c5d6712403be9c6d3e8bc0565495c4
thea-exe <csokkunthea@gmail.com>
2022-07-07 01:26:26 -0400

    moved to react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-        We Run ETH Events & kickstart new communities around the world
-        </p>
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+        <h1>
+        We Run ETH Events 
+        </h1>
+        <h2>
+      & kickstart new communities around the world
+        </h2>
       </header>
     </div>
   );
